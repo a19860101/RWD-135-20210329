203dd04d06fa3b030a52ced7a8c00b09994596fa
Zac <a19860101@gmail.com>
2021-05-17 19:48:05 +0800

    20210517

---
+++ layout/script.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.nav-collapse').hide();
+    $('.nav-toggle').click(function(){
+        // $('.nav-collapse').toggle(600);
+        $('.nav-collapse').slideToggle();
+    })
+})
