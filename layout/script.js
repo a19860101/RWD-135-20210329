532cdc7cac158f088a5882d171fe4c9e4644f7b8
Zac <a19860101@gmail.com>
2021-05-17 20:07:17 +0800

    20210517

--- layout/script.js
+++ layout/script.js
@@ -4,4 +4,12 @@ $(function(){
         // $('.nav-collapse').toggle(600);
         $('.nav-collapse').slideToggle();
     })
+    $('.modal-btn').click(function(){
+        let modal = $(this).attr('href');
+        $(modal).fadeIn();
+    })
+    $('.modal-close').click(function(){
+        $('.modal').fadeOut();
+    })
+    
 })
